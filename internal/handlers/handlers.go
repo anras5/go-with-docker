1104bfe8665a7b24f6398e3d26e2a3189acf12a9
anras5 <anras1filip@gmail.com>
2023-08-02 23:09:01 +0200

    json in home

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,7 +1,26 @@
 package handlers
 
-import "net/http"
+import (
+	"encoding/json"
+	"net/http"
+)
 
 func Home(w http.ResponseWriter, r *http.Request) {
-	_, _ = w.Write([]byte("Hello from the browser!"))
+	payload := struct {
+		Status  string `json:"status"`
+		Message string `json:"message"`
+		Version string `json:"version"`
+	}{
+		Status:  "active",
+		Message: "Go Todos API is up and running",
+		Version: "1.0.1",
+	}
+
+	// marshal the data
+	out, _ := json.Marshal(payload)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	_, _ = w.Write(out)
+
 }
